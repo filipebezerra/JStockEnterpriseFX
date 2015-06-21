493d19a61bcd32b9d203468cefd78be22495e21e
Filipe Bezerra <filipebzerra@gmail.com>
2015-06-21 12:16:16 -0300

    Criação de convensão sobre configuração no carregamento das views pela passagem de seu controller; Reimplementação da chamada dos menus.

---
+++ Workspace/JStockEnterpriseFX/src/jstockenterprisefx/JStockEnterpriseFXApplication.java
@@ -0,0 +1,56 @@
+package jstockenterprisefx;
+
+import java.io.IOException;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.BorderPane;
+import javafx.stage.Stage;
+import jstockenterprisefx.main.MainController;
+
+public class JStockEnterpriseFXApplication extends Application {
+	private static Scene sScene = null;
+	private static BorderPane sRoot = null;
+	
+	public static Scene getScene() {
+		if (sScene == null)
+			sScene = new Scene(JStockEnterpriseFXApplication.load(MainController.class));
+		return sScene;
+	}
+	
+	public static BorderPane getRoot() {
+		if (sRoot == null)
+			sRoot = (BorderPane) JStockEnterpriseFXApplication.getScene().getRoot();
+		return sRoot;
+	}
+
+	@Override
+	public void start(final Stage primaryStage) {
+		primaryStage.setScene(getScene());
+		primaryStage.show();
+	}
+	
+	public static <T> T load(final Class<?> viewControllerClass) {
+		final String simpleNameOfClass = viewControllerClass.getSimpleName();
+
+		String location = "";
+
+		if (!simpleNameOfClass.toLowerCase().endsWith("controller"))
+			return null;
+		else {
+			int indexOfViewName = simpleNameOfClass.toLowerCase().indexOf("controller");
+
+			String viewName = simpleNameOfClass.substring(0, indexOfViewName);
+
+			location = new StringBuffer(viewName).append("View.fxml").toString();
+		}
+
+		try {
+			return FXMLLoader.load(viewControllerClass.getResource(location));
+		} catch (IOException e) {
+			e.printStackTrace();
+			return null;
+		}
+	}
+}
--- Workspace/JStockEnterpriseFX/src/jstockenterprisefx/main/MainController.java
+++ Workspace/JStockEnterpriseFX/src/jstockenterprisefx/main/MainController.java
@@ -1,80 +1,75 @@
 package jstockenterprisefx.main;
 
-import java.io.IOException;
-
+import static jstockenterprisefx.JStockEnterpriseFXApplication.getRoot;
+import static jstockenterprisefx.JStockEnterpriseFXApplication.load;
+import javafx.beans.property.ObjectProperty;
+import javafx.beans.property.SimpleObjectProperty;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.layout.AnchorPane;
-import jstockenterprisefx.MainApp;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import jstockenterprisefx.department.DepartmentController;
+import jstockenterprisefx.group.GroupController;
+import jstockenterprisefx.item.ItemController;
+import jstockenterprisefx.stock.StockController;
+import jstockenterprisefx.supplier.SupplierController;
 
 public class MainController {
-	private MainApp mMainApp;
-
-	public MainController() {
-		super();
+	private static ObjectProperty<Parent> sGroupView = new SimpleObjectProperty<>(null);
+	private static ObjectProperty<Parent> sDepartmentView = new SimpleObjectProperty<>(null);
+	private static ObjectProperty<Parent> sItemView = new SimpleObjectProperty<>(null);
+	private static ObjectProperty<Parent> sSupplierView = new SimpleObjectProperty<>(null);
+	private static ObjectProperty<Parent> sStockView = new SimpleObjectProperty<>(null);
+	
+	private boolean isVisibleInCenter(final ObjectProperty<Parent> view) {
+		final Node center = getRoot().getCenter();
+		
+		if (view.get() == null)
+			throw new NullPointerException("View not initialized.");
+		
+		if (view.get().getId() == null)
+			throw new NullPointerException("View id not setted.");
+		
+		return (center != null && center.getId().equals(view.get().getId()));
 	}
-
-	public void setMainApp(final MainApp mainApp) {
-		mMainApp = mainApp;
+	
+	private void showInCenter(final ObjectProperty<Parent> view) {
+		if (! isVisibleInCenter(view))
+    		getRoot().setCenter(view.get());
 	}
-
+	
+	private void initializeView(final ObjectProperty<Parent> view, final Class<?> viewControllerClass) {
+		if (view.get() == null)
+			view.set(load(viewControllerClass));
+	}
+	
 	@FXML
-	private void showGroupView() {
-		try {
-			AnchorPane groupView = (AnchorPane) FXMLLoader.load(getClass()
-					.getResource("../group/GroupView.fxml"));
-			mMainApp.getMainView().setCenter(groupView);
-		} catch (IOException e) {
-			// TODO treat exception
-			e.printStackTrace();
-		}
-	}
+	private void showDepartamentView(final ActionEvent event) {
+		initializeView(sDepartmentView, DepartmentController.class);
+    	showInCenter(sDepartmentView);
+    }
 
-	@FXML
-	private void showDepartamentView() {
-		try {
-			AnchorPane departmentView = (AnchorPane) FXMLLoader.load(getClass()
-					.getResource("../department/DepartmentView.fxml"));
-			mMainApp.getMainView().setCenter(departmentView);
-		} catch (IOException e) {
-			// TODO treat exception
-			e.printStackTrace();
-		}
-	}
+    @FXML
+    private void showGroupView(final ActionEvent event) {
+    	initializeView(sGroupView, GroupController.class);
+    	showInCenter(sGroupView);
+    }
 
-	@FXML
-	private void showSupplierView() {
-		try {
-			AnchorPane supplierView = (AnchorPane) FXMLLoader.load(getClass()
-					.getResource("../supplier/SupplierView.fxml"));
-			mMainApp.getMainView().setCenter(supplierView);
-		} catch (IOException e) {
-			// TODO treat exception
-			e.printStackTrace();
-		}
-	}
+    @FXML
+    private void showItemView(final ActionEvent event) {
+    	initializeView(sItemView, ItemController.class);
+    	showInCenter(sItemView);
+    }
 
-	@FXML
-	private void showItemView() {
-		try {
-			AnchorPane itemView = (AnchorPane) FXMLLoader.load(getClass()
-					.getResource("../item/ItemView.fxml"));
-			mMainApp.getMainView().setCenter(itemView);
-		} catch (IOException e) {
-			// TODO treat exception
-			e.printStackTrace();
-		}
-	}
+    @FXML
+    private void showStockView(final ActionEvent event) {
+    	initializeView(sStockView, StockController.class);
+    	showInCenter(sStockView);
+    }
 
-	@FXML
-	private void showStockView() {
-		try {
-			AnchorPane stockView = (AnchorPane) FXMLLoader.load(getClass()
-					.getResource("../stock/StockView.fxml"));
-			mMainApp.getMainView().setCenter(stockView);
-		} catch (IOException e) {
-			// TODO treat exception
-			e.printStackTrace();
-		}
-	}
+    @FXML
+    private void showSupplierView(final ActionEvent event) {
+    	initializeView(sSupplierView, SupplierController.class);
+    	showInCenter(sSupplierView);
+    }
 }
