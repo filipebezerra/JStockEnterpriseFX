6071cf5a221d3f754c1fb2e75de531be508e7c5b
Filipe Bezerra <filipebzerra@gmail.com>
2015-06-25 13:50:20 -0300

    Correção do tipo do campo Uf da entidade.

--- Workspace/JStockEnterpriseFX/src/jstockenterprisefx/supplier/Supplier.java
+++ Workspace/JStockEnterpriseFX/src/jstockenterprisefx/supplier/Supplier.java
@@ -5,10 +5,13 @@ import java.io.Serializable;
 import javax.persistence.Basic;
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
 import javax.persistence.NamedQueries;
 import javax.persistence.NamedQuery;
 
 import jstockenterprisefx.base.entity.BaseEntity;
+import jstockenterprisefx.base.model.Uf;
 
 /**
  *
@@ -16,182 +19,185 @@ import jstockenterprisefx.base.entity.BaseEntity;
  */
 @Entity
 @NamedQueries({
-    @NamedQuery(name = "Supplier.findAll", query = "SELECT s FROM Supplier s"),
-    @NamedQuery(name = "Supplier.findById", query = "SELECT s FROM Supplier s WHERE s.id = :id"),
-    @NamedQuery(name = "Supplier.findByCompanyName", query = "SELECT s FROM Supplier s WHERE s.companyName = :companyName")})
+		@NamedQuery(name = "Supplier.findAll", query = "SELECT s FROM Supplier s"),
+		@NamedQuery(name = "Supplier.findById", query = "SELECT s FROM Supplier s WHERE s.id = :id"),
+		@NamedQuery(name = "Supplier.findByCompanyName", query = "SELECT s FROM Supplier s WHERE s.companyName = :companyName") })
 public class Supplier extends BaseEntity<Integer> implements Serializable {
-    private static final long serialVersionUID = 1L;
-    
-    @Basic(optional = false)
-    @Column(nullable = false, length = 100)
-    private String companyName;
-    
-    @Basic(optional = false)
-    @Column(nullable = false, length = 120)
-    private String tradingName;
-    
-    @Basic(optional = false)
-    @Column(nullable = false, length = 14, unique = true)
-    private String cnpj;
-    
-    @Column(length = 11)
-    private String phoneNumber;
-    
-    @Column(length = 120, unique = true)
-    private String emailAddress;
-    
-    @Basic(optional = true)
-    @Column(nullable = true, length = 100)
-    private String publicArea;
-    
-    @Basic(optional = true)
-    @Column(nullable = true, length = 80)
-    private String district;
-    
-    @Basic(optional = true)
-    @Column(nullable = true, length = 80)
-    private String city;
-    
-    @Basic(optional = true)
-    @Column(nullable = true, length = 2)
-    private String uf;
-    
-    @Basic(optional = false)
-    @Column(nullable = false, length = 8)
-    private String cep;
+	private static final long serialVersionUID = 1L;
 
-    public Supplier() {
-    }
+	@Basic(optional = false)
+	@Column(nullable = false, length = 100)
+	private String companyName;
 
-    public Supplier(final Integer id) {
-        this.id = id;
-    }
-    
-    public Supplier(final String companyName, final String tradingName,
-    		final String cnpj, final String cep) {
-        this(null, companyName, tradingName, cnpj, null, null, null, null, cep);
-    }
-    
-    public Supplier(final String companyName, final String tradingName,
-    		final String cnpj, final String publicArea, final String district, final String city,
-    		final String uf, final String cep) {
-        this(null, companyName, tradingName, cnpj, publicArea, district, city, uf, cep);
-    }
+	@Basic(optional = false)
+	@Column(nullable = false, length = 120)
+	private String tradingName;
 
-    public Supplier(final Integer id, final String companyName, final String tradingName,
-    		final String cnpj, final String publicArea, final String district, final String city,
-    		final String uf, final String cep) {
-        this.id = id;
-        this.companyName = companyName;
-        this.tradingName = tradingName;
-        this.cnpj = cnpj;
-        this.publicArea = publicArea;
-        this.district = district;
-        this.city = city;
-        this.uf = uf;
-        this.cep = cep;
-    }
+	@Basic(optional = false)
+	@Column(nullable = false, length = 14, unique = true)
+	private String cnpj;
 
-    public String getCompanyName() {
-        return companyName;
-    }
+	@Column(length = 11)
+	private String phoneNumber;
 
-    public void setCompanyName(final String companyName) {
-        this.companyName = companyName;
-    }
+	@Column(length = 120, unique = true)
+	private String emailAddress;
 
-    public String getTradingName() {
-        return tradingName;
-    }
+	@Basic(optional = true)
+	@Column(nullable = true, length = 100)
+	private String publicArea;
 
-    public void setTradingName(final String tradingName) {
-        this.tradingName = tradingName;
-    }
+	@Basic(optional = true)
+	@Column(nullable = true, length = 80)
+	private String district;
 
-    public String getCnpj() {
-        return cnpj;
-    }
+	@Basic(optional = true)
+	@Column(nullable = true, length = 80)
+	private String city;
 
-    public void setCnpj(final String cnpj) {
-        this.cnpj = cnpj;
-    }
+	@Enumerated(EnumType.STRING)
+	@Column(nullable = true, length = 2)
+	private Uf uf;
 
-    public String getPhoneNumber() {
-        return phoneNumber;
-    }
+	@Basic(optional = false)
+	@Column(nullable = false, length = 8)
+	private String cep;
 
-    public void setPhoneNumber(final String phoneNumber) {
-        this.phoneNumber = phoneNumber;
-    }
+	public Supplier() {
+	}
 
-    public String getEmailAddress() {
-        return emailAddress;
-    }
+	public Supplier(final Integer id) {
+		this.id = id;
+	}
 
-    public void setEmailAddress(final String emailAddress) {
-        this.emailAddress = emailAddress;
-    }
+	public Supplier(final String companyName, final String tradingName,
+			final String cnpj, final String cep) {
+		this(null, companyName, tradingName, cnpj, null, null, null, null, cep);
+	}
 
-    public String getPublicArea() {
-        return publicArea;
-    }
+	public Supplier(final String companyName, final String tradingName,
+			final String cnpj, final String publicArea, final String district,
+			final String city, final Uf uf, final String cep) {
+		this(null, companyName, tradingName, cnpj, publicArea, district, city,
+				uf, cep);
+	}
 
-    public void setPublicArea(final String publicArea) {
-        this.publicArea = publicArea;
-    }
+	public Supplier(final Integer id, final String companyName,
+			final String tradingName, final String cnpj,
+			final String publicArea, final String district, final String city,
+			final Uf uf, final String cep) {
+		this.id = id;
+		this.companyName = companyName;
+		this.tradingName = tradingName;
+		this.cnpj = cnpj;
+		this.publicArea = publicArea;
+		this.district = district;
+		this.city = city;
+		this.uf = uf;
+		this.cep = cep;
+	}
 
-    public String getDistrict() {
-        return district;
-    }
+	public String getCompanyName() {
+		return companyName;
+	}
 
-    public void setDistrict(final String district) {
-        this.district = district;
-    }
+	public void setCompanyName(final String companyName) {
+		this.companyName = companyName;
+	}
 
-    public String getCity() {
-        return city;
-    }
+	public String getTradingName() {
+		return tradingName;
+	}
 
-    public void setCity(final String city) {
-        this.city = city;
-    }
+	public void setTradingName(final String tradingName) {
+		this.tradingName = tradingName;
+	}
 
-    public String getUf() {
-        return uf;
-    }
+	public String getCnpj() {
+		return cnpj;
+	}
 
-    public void setUf(final String uf) {
-        this.uf = uf;
-    }
+	public void setCnpj(final String cnpj) {
+		this.cnpj = cnpj;
+	}
 
-    public String getCep() {
-        return cep;
-    }
+	public String getPhoneNumber() {
+		return phoneNumber;
+	}
 
-    public void setCep(final String cep) {
-        this.cep = cep;
-    }
+	public void setPhoneNumber(final String phoneNumber) {
+		this.phoneNumber = phoneNumber;
+	}
 
-    @Override
-    public int hashCode() {
-        int hash = 0;
-        hash += (id != null ? id.hashCode() : 0);
-        return hash;
-    }
+	public String getEmailAddress() {
+		return emailAddress;
+	}
 
-    @Override
-    public boolean equals(final Object object) {
-        if (!(object instanceof Supplier))
+	public void setEmailAddress(final String emailAddress) {
+		this.emailAddress = emailAddress;
+	}
+
+	public String getPublicArea() {
+		return publicArea;
+	}
+
+	public void setPublicArea(final String publicArea) {
+		this.publicArea = publicArea;
+	}
+
+	public String getDistrict() {
+		return district;
+	}
+
+	public void setDistrict(final String district) {
+		this.district = district;
+	}
+
+	public String getCity() {
+		return city;
+	}
+
+	public void setCity(final String city) {
+		this.city = city;
+	}
+
+	public Uf getUf() {
+		return uf;
+	}
+
+	public void setUf(final Uf uf) {
+		this.uf = uf;
+	}
+
+	public String getCep() {
+		return cep;
+	}
+
+	public void setCep(final String cep) {
+		this.cep = cep;
+	}
+
+	@Override
+	public int hashCode() {
+		int hash = 0;
+		hash += (id != null ? id.hashCode() : 0);
+		return hash;
+	}
+
+	@Override
+	public boolean equals(final Object object) {
+		if (!(object instanceof Supplier))
 			return false;
-        Supplier other = (Supplier) object;
-        if ((id == null && other.id != null) || (id != null && !id.equals(other.id)))
+		Supplier other = (Supplier) object;
+		if ((id == null && other.id != null)
+				|| (id != null && !id.equals(other.id)))
 			return false;
-        return true;
-    }
+		return true;
+	}
 
-    @Override
-    public String toString() {
-        return companyName;
-    }
-    
+	@Override
+	public String toString() {
+		return companyName;
+	}
+
 }
--- Workspace/JStockEnterpriseFX/test/jstockenterprisefx/supplier/SupplierJpaPersistenceTest.java
+++ Workspace/JStockEnterpriseFX/test/jstockenterprisefx/supplier/SupplierJpaPersistenceTest.java
@@ -3,6 +3,7 @@ package jstockenterprisefx.supplier;
 import static org.junit.Assert.assertEquals;
 import jstockenterprisefx.base.jpa.BaseJpaPersistenceTest;
 import jstockenterprisefx.base.jpa.JpaEntityManager;
+import jstockenterprisefx.base.model.Uf;
 
 import org.junit.Test;
 
@@ -16,7 +17,8 @@ public class SupplierJpaPersistenceTest extends
 	@Test
 	public void testSupplierCrud() {
 		Supplier newSupplier = new Supplier("The Creative Shop",
-				"The Creative Shop S.A.", "14877648000155", "21393054");
+				"The Creative Shop S.A.", "14877648000155", "Av. R2",
+				"Bairro Conceição", "São Paulo", Uf.SP, "21393054");
 
 		Supplier insertedSupplier = testCreate(newSupplier);
 
@@ -26,12 +28,19 @@ public class SupplierJpaPersistenceTest extends
 				"The Creative Shop S.A.");
 		assertEquals("Insert fail", insertedSupplier.getCnpj(),
 				"14877648000155");
+		assertEquals("Insert fail", insertedSupplier.getPublicArea(), "Av. R2");
+		assertEquals("Insert fail", insertedSupplier.getDistrict(),
+				"Bairro Conceição");
+		assertEquals("Insert fail", insertedSupplier.getCity(), "São Paulo");
+		assertEquals("Insert fail", insertedSupplier.getUf(), Uf.SP);
 		assertEquals("Insert fail", insertedSupplier.getCep(), "21393054");
 
 		insertedSupplier.setCompanyName("APN Publicidade");
 		insertedSupplier.setTradingName("APN Publicidade Lda");
 		insertedSupplier.setCnpj("25183681000100");
-		insertedSupplier.setCep("12148608");
+		insertedSupplier.setCep("74691852");
+		insertedSupplier.setCity("Goiânia");
+		insertedSupplier.setUf(Uf.GO);
 
 		Supplier updatedSupplier = testUpdate(insertedSupplier);
 
@@ -41,7 +50,9 @@ public class SupplierJpaPersistenceTest extends
 				"APN Publicidade Lda");
 		assertEquals("Update fail", insertedSupplier.getCnpj(),
 				"25183681000100");
-		assertEquals("Update fail", insertedSupplier.getCep(), "12148608");
+		assertEquals("Update fail", insertedSupplier.getCity(), "Goiânia");
+		assertEquals("Update fail", insertedSupplier.getUf(), Uf.GO);
+		assertEquals("Update fail", insertedSupplier.getCep(), "74691852");
 
 		testList(1);
 
